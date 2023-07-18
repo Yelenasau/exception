c0a8c29d99e97ffb1deeda75c6886622d0beaf55
Yelenasau <elenasauschkina@mail.ru>
2023-07-18 19:18:18 +0400

    5

--- src/Validator.java
+++ src/Validator.java
@@ -2,34 +2,51 @@ public class Validator {
     private static final String ALLOWED_CHARS = "abcdefghijklmnopqrstuvwxyz0123456789_";
 
     public static boolean check(String login, String password, String confirmPassword) throws WrongLoginException, WrongPasswordException {
-        isValidChars();
-        checkAlphabet();
+        checkLogin(login);
+        checkPassword(password);
+        checkAlphabet(login);
+        checkAlphabet(password);
+        checkConfirmPassword(password, confirmPassword);
 
         return true;
     }
 
     private static boolean checkAlphabet(String str) {
-            for (char symbol : str.toCharArray()) {
+        for (char symbol : str.toCharArray()) {
 
-                if (!ALLOWED_СHARS.contains(Character.toString(symbol))) {
+            if (!ALLOWED_CHARS.contains(Character.toString(symbol))) {
 
-                    return false;
-                }
+                return false;
             }
-
-            return true;
         }
 
-    private static boolean isValidChars(String login, String password ) throws WrongLoginException, WrongPasswordException {
-            if (login.length() > 20 || !checkAlphabet(login)) {
+        return true;
+    }
 
-                throw new WrongLoginException("Логин содержит недопустимые символы!");
-            }
-            if (password.length() > 20 || !checkAlphabet(password)) {
+    private static void checkLogin(String login) throws WrongLoginException {
 
-            throw new WrongPasswordException("Пароль  содержит недопустимые символы!");
+        if (login.length() > 20 || !checkAlphabet(login)) {
+
+
+            throw new WrongLoginException("Логин содержит недопустимые символы!");
 
         }
     }
+
+    private static void checkPassword(String password) throws WrongPasswordException {
+
+        if (password.length() > 20 || !checkAlphabet(password)) {
+
+            throw new WrongPasswordException(" Пароль содержит недопустимые символы!");
+        }
     }
 
+    private static void checkConfirmPassword(String password, String confirmPassword) throws WrongPasswordException {
+
+        if (!password.equals(confirmPassword)) {
+
+            throw new WrongPasswordException(" Пароли не совпадают!");
+
+        }
+    }
+}
